716292969d0c53dcedc9f3cd2e1d2bacbf81d4d7
Abhay Tank <iabhaytank@gmail.com>
2026-08-20 18:32:45 +0530

    Remove destination schema from Sanity Studio

--- sanity-studio/schemas/index.js
+++ sanity-studio/schemas/index.js
@@ -1,11 +1,9 @@
 import trek from './trek'
-import destination from './destination'
 import testimonial from './testimonial'
 import settings from './settings'
 
 export const schemaTypes = [
   trek,
-  destination,
   testimonial,
   settings
 ]
